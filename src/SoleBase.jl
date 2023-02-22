41672b5c4066e6bd0fd4930b7e29a5319f0e79b5
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-02-22 18:32:55 +0100

    Docu

--- src/SoleBase.jl
+++ src/SoleBase.jl
@@ -1,10 +1,21 @@
 module SoleBase
 
 """
+    abstract type AbstractDataset end
+
 Abstract supertype for all datasets.
+
+See also [`nsamples`](@ref).
 """
 abstract type AbstractDataset end
 
+"""
+    nsamples(X::AbstractDataset)
+
+Number of samples in the dataset.
+
+See also [`AbstractDataset`](@ref).
+"""
 function nsamples(X::AbstractDataset)
     error("Please, provide method nsamples(::$(typeof(X))).")
 end
