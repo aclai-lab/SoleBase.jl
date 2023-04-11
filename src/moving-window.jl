9b6e3eecb34d9f10d4bc4e9404bd18987d7e4e11
lorebalbo <lorenzo02.balboni@edu.unife.it>
2023-04-11 14:37:41 +0200

    moving window - window_size and window_step

--- src/moving-window.jl
+++ src/moving-window.jl
@@ -3,7 +3,10 @@
 # moving window
 
 """
-TODO: docs
+    moving_window(npoints, nwindows, relative_overlap, window_size, window_step; kwargs...)
+
+Generates a certain number of windows from a time serie, and returns them as a Vector of
+indices.
 """
 function moving_window(
         npoints::Integer;
@@ -94,13 +97,12 @@ Return a certain number of windows where each window as length `window_size` and
 between each window is `window_step`.
 
 When a `landmark` is passed to the function, each of the generated windows will have a point
-in common, the one indicated by `landmark`.
+in common, the one indicated by `landmark`. For example, if the time serie has 100 points and
+the landmark is 50, all the generated windows will have the 50th point.
+Additionally, is possible to specify the position of the landmark in the generated window
+using `allow_landmark_position`.
 
-`allow_landmark_on_edges` allow to discard or not those windows where the landmark is on the
-first or last point.
-Note that in some cases (still to define) setting `allow_landmark_on_edges` to false will
-generate no window due to the used method of windows generation. In these cases, is possible
-to force the generation of the windows by setting `force` to true (still to implement).
+
 
 Note: the step between two window is the distance between the first point of a window and
 the first point of the window next to it.
@@ -112,24 +114,25 @@ function _moving_window(
         window_step::Integer;
         landmark::Union{Integer,Nothing} = nothing,
         allow_landmark_position::Tuple{<:AbstractFloat,<:AbstractFloat} = (0.0, 1.0),
-        # TODO: refactor `allow_landmark_on_edges` to accept a range of accepted relative
-        #         positions the landmark can be in
-        allow_landmark_on_edges::Bool = true,
         allow_overflow = false,
-        # TODO: consider whether this is needed or not: force::Bool = false
     )::AbstractVector{UnitRange{Int}}
-
-    # f = open("aatest.txt", "a+")
-    # TODO: following asserts:
-    #  - window_step < window_size in each cases? even with allow_overflow setted to true?
-    #  - allow_landmark_on_edges true, then landmark != 1 && landmark != npoints && window_size > 3
-    #    if allow_overflow then landmark can be equal to npoints. Even landmark == 1?
-    #  -
+    if first(allow_landmark_position) > last(allow_landmark_position)
+        throw(ArgumentError(
+            string("allow_landmark_position must have the second element greater than the
+            first one. Got $(first(allow_landmark_position)) >
+            $(last(allow_landmark_position))")
+        ))
+    end
+    if !(first(allow_landmark_position) in 0.000:0.001:1.000) || !(last(allow_landmark_position) in 0.000:0.001:1.000)
+        throw(ArgumentError(
+            string("element of allow_landmark_position must be in range 0.000:0.001:1.000. Got
+            $(first(allow_landmark_position)) > * $(last(allow_landmark_position))")
+        ))
+    end
 
     start = !isnothing(landmark) ? landmark-window_size+1 : 1
-    # start = landmark == window_size ? start + 1 : start
     start = start < 1 ? 1 : start
-    indices = map((r)->r:r+window_size-1, range(start, npoints-(window_size-1), step = window_step))
+    indices = map((r)->r:r+window_size-1, range(start, npoints, step = window_step))
 
     if !allow_overflow
         filter!((w)->w.start in 1:npoints && w.stop in 1:npoints, indices)
@@ -139,46 +142,10 @@ function _moving_window(
         filter!(x->landmark in x, indices)
 
         if allow_landmark_position != (0.0, 1.0)
-            filter!(x->!(landmark in first(x):round(Int, length(x) * first(allow_landmark_position))), indices)
-            filter!(x->!(landmark in round(Int, length(x) * (1 - last(allow_landmark_position))):last(x)), indices)
-        end
-
-        if !allow_landmark_on_edges
-            if isempty(filter(x->(first(x) != landmark && last(x) != landmark), indices))
-                if length(indices) == 1
-                    # TODO: DO WE WANT THE FUNCTION TO RETURN AN EMPTY VECTOR IN SOME CASES? if yes the following lines
-                    # are not necessary and so the following todos.
-
-                    #println(f, string("npoints: ", npoints, " window_size: ", window_size, " window_step: ", window_step, " landmark: ", landmark))
-                    # window_step > window_size
-                    if indices[1][1] == landmark
-                        @test start + window_step + window_size - 1 in npoints == false
-                        # TODO: find for which combinations of npoints, window_size, window_step, landmark, ecc. we
-                        # end up here and put an assert at the top of the function. If force is true, that assert will
-                        # not be considdered
-                        indices = [indices[1][1]-1:indices[1][end]-1]
-                    elseif indices[1][end] == landmark
-                        # TODO: find for which combinations of npoints, window_size, window_step, landmark, ecc. we
-                        # end up here and put an assert at the top of the function. If force is true, that assert will
-                        # not be considdered
-                        indices = [indices[1][1]+1:indices[1][end]+1]
-                    end
-                elseif length(indices) == 2
-                    # TODO: find for which combinations of npoints, window_size, window_step, landmark, ecc. we
-                    # end up here and put an assert at the top of the function. If force is true, that assert will
-                    # not be considdered
-                    indices = [
-                        indices[1][1]+1:indices[1][end]+1,
-                        indices[2][1]+1:indices[2][end]+1
-                    ]
-                    # @show npoints, window_size, window_step, landmark
-                end
-            else
-                filter!(x->(first(x) != landmark && last(x) != landmark), indices)
-            end
+            landmark_positions = map((i)->(findfirst(x->x==landmark, i))/length(i), indices)
+            indices = indices[findall(l->round(l, digits = 1) in allow_landmark_position[1]:0.001:allow_landmark_position[2], landmark_positions)]
         end
     end
-    # close(f)
 
     indices
 end
