58aec744d2c94802933e5e664b700937dd35a203
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-06-07 19:06:58 +0200

    slice_dataset->slicedataset,instances

--- src/SoleBase.jl
+++ src/SoleBase.jl
@@ -20,6 +20,65 @@ See also [`ninstances`](@ref).
 """
 abstract type AbstractDataset end
 
+
+_doc_slicedataset = """
+    function slicedataset(
+        dataset::D,
+        dataset_slice::AbstractVector{<:Integer};
+        allow_no_instances = false,
+        return_view = false,
+        kwargs...,
+    )::D where {D<:AbstractDataset}
+
+Return a machine learning dataset with a subset of the instances.
+
+# Implementation
+
+In order to use slicedataset on a custom dataset representation,
+provide the following method:
+    instances(
+        dataset::D,
+        dataset_slice::AbstractVector{<:Integer},
+        return_view::Union{Val{true},Val{false}};
+        kwargs...
+    )::D where {D<:AbstractDataset}
+"""
+
+"""$(_doc_slicedataset)"""
+function slicedataset(
+    dataset::D,
+    dataset_slice::Union{Colon,Integer,AbstractVector{<:Integer},Tuple{<:Integer}};
+    allow_no_instances = false,
+    return_view = false,
+    kwargs...,
+)::D where {D}
+    if dataset_slice isa Integer
+        dataset_slice = [dataset_slice]
+    elseif dataset_slice isa Tuple{<:Integer}
+        dataset_slice = collect(dataset_slice)
+    end
+    if dataset_slice isa Colon
+        return deepcopy(dataset)
+    else
+        @assert (allow_no_instances ||
+            (!(dataset_slice isa Union{AbstractVector{<:Integer},Tuple{<:Integer}}) ||
+                length(dataset_slice) > 0)) "Can't apply empty slice to dataset."
+        return instances(dataset, dataset_slice, Val(return_view); kwargs...)
+    end
+end
+
+"""$(_doc_slicedataset)"""
+function instances(
+    dataset::D,
+    dataset_slice::AbstractVector{<:Integer},
+    return_view::Union{Val{true},Val{false}};
+    kwargs...
+)::D where {D}
+    return error("`instances` method not implemented for type "
+        * string(typeof(dataset))) * "."
+end
+
+
 # -------------------------------------------------------------
 # AbstractDataset - ninstances
 
