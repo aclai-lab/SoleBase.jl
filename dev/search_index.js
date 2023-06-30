84c2db11e33729e26c8dd0ce9bf3a8ebcc731681
Documenter.jl <documenter@juliadocs.github.io>
2023-06-30 08:49:57 +0000

    build based on 6f9ac1e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SoleBase","category":"page"},{"location":"#SoleBase","page":"Home","title":"SoleBase","text":"","category":"section"}]
+}
