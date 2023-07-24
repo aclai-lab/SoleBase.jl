483395a4b8e5a373a10857bf0177f89067b85994
lorebalbo <lorenzo02.balboni@edu.unife.it>
2023-07-24 12:05:17 +0200

    movingwindow fixed length tests fix

--- test/movingwindow/fixedlength.jl
+++ test/movingwindow/fixedlength.jl
@@ -3,30 +3,29 @@ for npoints in 1:N
         for window_step in 1:npoints
             indices = movingwindow(npoints; window_length = window_length, window_step = window_step)
             indices_coverage = movingwindow(npoints; window_length = window_length, window_step = window_step, force_coverage = true)
-            for ids in [indices, indices_coverage[1:end-1]]
-                # window_length
-                @test length(unique([length(ids) for ids in indices])) == 1
-                @test all([length(i) == window_length for i in indices]) == true
+
+            # @show indices, indices_coverage
+            # @show indices
+
+            # window_length
+            @test length(unique([length(ids) for ids in indices])) == 1
+            @test all([length(i) == window_length for i in indices]) == true
+
+            for indices_ in [indices, indices_coverage]
                 # window_step
-                @test all([first(indices[i+1]) - first(indices[i]) == window_step for i in 1:length(indices)-1]) == true
+                @test all([first(indices_[i+1]) - first(indices_[i]) == window_step for i in 1:length(indices_)-1]) == true
                 # others
-                @test first(first(indices)) == 1
+                @test first(first(indices_)) == 1
+                @test all([first(i) in 1:npoints for i in indices_]) == true
             end
 
-            # Check if a window as been filtered out due overbound
-            if (first(last(indices)) + window_step) <= npoints
-                # force_coverage
-                @test last(last(indices_coverage)) == npoints
-            end
+            # above tests
+            @test isempty(setdiff(indices, indices_coverage)) == true
 
-            # for landmark in 1:npoints
-            #     indices = movingwindow(npoints; window_length = window_length, nwindows = nwindows, landmark = landmark)
-            #     # window_length
-            #     @test all(length.(indices) .== length(first(indices))) == true
-            #     # nwindows
-            #     @test length(indices) == nwindows
-            #     # landmark
-            #     @test all(map((i)->landmark in i,indices)) == true
+            # force_coverage
+            if indices != indices_coverage
+                @test first(unique([last(ids) for ids in setdiff(indices_coverage, indices)])) == npoints
+            end
         end
     end
 end
